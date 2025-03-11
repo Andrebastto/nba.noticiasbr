1de4837bc37ed44549aeb838ce5d4bfbc3740818
Andrebastto <andrelnsb@gmail.com>
2025-03-10 21:03:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function verificarResposta(pergunta) {
+    var respostas = document.getElementsByName('answer' + pergunta);
+    var resultado = document.getElementById('resultado' + pergunta);
+    var respostaSelecionada = null;
+
+    
+}
+
